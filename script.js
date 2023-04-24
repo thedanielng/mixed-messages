2332e23c60611b2641cc593913bef2fa32377343
thedanielng <92590828+thedanielng@users.noreply.github.com>
2023-04-24 17:11:30 +0200

    modified the script to only print one sentence at a time

--- script.js
+++ script.js
@@ -1,29 +1,22 @@
-let randomNumber = (num) => {
-    return Math.floor(Math.random() * num);
-};
-
 let universeChar = {
     marvel: ['Iron Man', 'Captain America', 'Thor', 'Black Widow', 'Hawkeye', 'Doctor Strange', 'Spider-Man', 'Black Panther'],
     dc: ['Superman', 'Batman', 'Wonder Woman', 'The Flash', 'Cyborg', 'Shazam', 'Aquaman', 'Green Lantern'],
     harrypotter: ['Harry Potter', 'Hermione Granger', 'Ron Weasley', 'Ginny Weasley', 'Draco Malfoy', 'Luna Lovegood', 'Albus Dumbledore', 'Remus Lupin']
-}
-
-let theUniverse = [];
-
-for(let char in universeChar){
-    switch(char){
-        case 'marvel':
-            theUniverse.push(`Your recruiter to the Avengers would be ${universeChar[char][randomNumber(universeChar[char].length)]}.`);
-            break;
-        case 'dc':
-            theUniverse.push(`Your enemy in the DC Universe would be ${universeChar[char][randomNumber(universeChar[char].length)]}.`);
-            break;
-        case 'harrypotter':
-            theUniverse.push(`You'll be cursed by ${universeChar[char][randomNumber(universeChar[char].length)]}.`);
-            break;
-    }
-};
-
-let message = theUniverse.join('\n');
-
-console.log(message);
+  };
+  
+  let keys = Object.keys(universeChar);
+  let randomIndex = Math.floor(Math.random() * keys.length);
+  let randomKey = keys[randomIndex];
+  let randomChar = universeChar[randomKey][Math.floor(Math.random() * universeChar[randomKey].length)];
+  
+  switch(randomKey) {
+    case 'marvel': 
+      console.log(`Your recruiter to the Avengers is ${randomChar}.`);
+      break;
+    case 'dc': 
+      console.log(`Your recruiter to the Justice League is ${randomChar}.`);
+      break;
+    case 'harrypotter':
+      console.log(`Your recruiter to Hogwarts is ${randomChar}.`);
+      break;
+  }
